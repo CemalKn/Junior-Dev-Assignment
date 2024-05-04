c9bc5ae0bc40bb988a3fefbba03037cc2722088b
Cemal Kırtan <leader58619@hotmail.com>
2024-05-04 16:45:44 +0300

    Initial commit

---
+++ src/main/java/com/student/JuniorDevAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorDevAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorDevAssignmentApplication.class, args);
+	}
+
+}
